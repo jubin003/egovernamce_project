8b95f78dd025a02c6dc1efdc664e18adae2f6860
sujal-st <sujal.sthapit4@gmail.com>
2025-11-24 10:31:09 +0545

    revised login

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -2,11 +2,12 @@ import React, { Children } from 'react'
 import {Navigate} from "react-router-dom"
 
 function ProtectedRoute() {
-    const isLoggegIn = localStorage.getItem("adminToken");
-    if(!isLoggegIn){
+    const isLoggedIn = localStorage.getItem("adminToken");
+    //!isLoggedIn
+    if(!isLoggedIn){
         return <Navigate to="/admin/login" replace />
     }
-    return children;
+    return Children;
 }
 
 export default ProtectedRoute
